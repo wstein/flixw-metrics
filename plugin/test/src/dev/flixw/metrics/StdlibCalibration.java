74c850c111616065181c7c157c4f6e5821805f18
Werner Stein <werner.stein@gmail.com>
2026-09-17 14:43:34 +0200

    fix: calibrate the current Flix adapter

--- plugin/test/src/dev/flixw/metrics/StdlibCalibration.java
+++ plugin/test/src/dev/flixw/metrics/StdlibCalibration.java
@@ -1,6 +1,6 @@
 package dev.flixw.metrics;
 
-import dev.flixw.metrics.adapter.Flix0753Adapter;
+import dev.flixw.metrics.adapter.Flix0761Adapter;
 import dev.flixw.metrics.sdk.CompilerModel.Model;
 
 import java.nio.file.Path;
@@ -17,7 +17,7 @@ public final class StdlibCalibration {
         Path project = Path.of(args[0]).toAbsolutePath().normalize();
         Path source = Path.of(args[1]).toAbsolutePath().normalize();
         MetricsConfig config = MetricsConfig.defaults();
-        Model model = new Flix0753Adapter().measureCompilerSource(project, args[2]);
+        Model model = new Flix0761Adapter().measureCompilerSource(project, args[2]);
         SourceMetrics text = SourceMetrics.measure(source.getParent(), List.of(source), config);
         Metrics.Report report = Metrics.of(1, model, text, config);
         System.out.print(report.render(Metrics.Format.JSON, null, config));
